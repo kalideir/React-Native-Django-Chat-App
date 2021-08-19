830e4d5aea58e7641ce473d91f1193933bfac038
Ali H. Dulaimi <43238519+Decoder3-14@users.noreply.github.com>
2021-08-20 02:09:52 +0300

    docker + chats works

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -198,7 +198,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('redis', 6379)],
         },
     },
 }
